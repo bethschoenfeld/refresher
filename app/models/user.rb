445b0853a8fb32b0752ad848d08c3ec6a911ccf3
bethschoenfeld <bethingraham3@gmail.com>
2018-04-30 13:29:22 -0400

    Seeds and models built out in rails

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+
+  has_many :posts, dependent: :destroy
+  has_many :cities, through: :posts
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,44 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+City.destroy_all
+Post.destroy_all
+
+ActiveRecord::Base.connection.tables.each do |t|
+  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+end
+
+beth = User.create(
+  username: 'bschoe',
+  firstname: 'Beth',
+  lastname: 'Schoenfeld',
+  img_url: 'https://i.imgur.com/prnhAfX.jpg',
+  description: 'Wooow.'
+)
+
+hughesville = City.create(
+  name: 'Hughesville',
+  location: 'Pennyslvania',
+  img_url: 'http://i0.kym-cdn.com/entries/icons/original/000/021/521/DumpsterFire2.jpg'
+)
+
+Post.create(
+  title: 'This is a typical small town.',
+  content: 'There is one light in this town. There is an amazing diner.',
+  user: beth, 
+  city: hughesville
+)
+
+chris = User.create(username: 'moodyc21', firstname: 'Christopher', lastname: 'Moody', img_url: 'https://i.imgur.com/XT6bwJI.jpg?1', description: 'Something, some-thing.')
+atlanta = City.create(name: 'Atlanta, GA', location: 'Georgia, USA', img_url: 'https://i.imgur.com/7i2UEAT.jpg')
+Post.create(title: 'Atlanta is Great!', content: 'wow.', user: chris, city: atlanta)
+
+olee = User.create(username: 'babylee2002', firstname: 'Okhyun', lastname: 'Lee', img_url: 'https://i.imgur.com/kcDlE28.jpg', description: 'Hey!')
+singapore = City.create(name: 'Singapore', location: 'Island off of Malaysia', img_url: 'https://i.imgur.com/H1rbRh8.jpg')
+Post.create(title: 'Singapore was not a bore!', content: '.', user: olee, city: singapore)
+
+josh = User.create(username: 'jschoe04', firstname: 'Josh', lastname: 'Schoenfeld', img_url: 'https://i.imgur.com/B0KOrC1.jpg?2', description: 'Hi!')
+barcelona = City.create(name: 'Barcelona', location: 'Spain', img_url: 'https://i.imgur.com/9h4qO3c.jpg?1')
+Post.create(title: 'Barcelona I do not want to come homa!', content: 'm.', user: josh, city: barcelona)
+
+murdock = User.create(username: 'mDawg1234', firstname: 'Murdock', lastname: 'Schoenfeld', img_url: 'https://i.imgur.com/bFFkBgF.png', description: 'Bark!')
+newZealand = City.create(name: 'New Zealand', location: 'Off the coast of Australia', img_url: 'https://i.imgur.com/Q2MzDFx.jpg')
+Post.create(title: 'New Zealand was alright.', content: '.', user: murdock, city: newZealand)
